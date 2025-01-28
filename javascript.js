977e8d3905ae6f095a958acf81f59a2a291b2028
klpabes <121295353+klpabes@users.noreply.github.com>
2025-01-28 11:49:16 +0800

    Add function to get computer choice

---
+++ javascript.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    return choices[Math.floor((Math.random() * 3) + 1)];
+}
+
+function getHumanChoice() {
+    let choice = prompt('Choose between rock, paper, scissors').toLowerCase();
+    if (choice) {
+        choice = choice.toLowerCase();
+        if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+            return choice;
+    }
+        else {
+            alert('Choose between rock, paper, and scissors');
+            return;
+        }
+    } else {
+        alert('Cancelled');
+        return;
+    }
+}
